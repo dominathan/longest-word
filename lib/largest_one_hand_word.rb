1fbc48a2202b40a01542a990d7172703d48396f4
Nathan Hall <nathan.mh@gmail.com>
2015-01-14 13:53:55 -0600

    minified the elements

--- lib/largest_one_hand_word.rb
+++ lib/largest_one_hand_word.rb
@@ -1,5 +1,6 @@
 # Given a dictionary, what is the largest word you can type using only your left hand
 require_relative '../tale_of_two_cities.rb'
+require 'pry'
 
 class LargestWord
   attr_reader :hands
@@ -12,39 +13,36 @@ class LargestWord
 
   #Check to see if right hand can type one word
   def type_with_right_hand?(word)
-    word.downcase.scan(Regexp.new(@hands[:left])).empty?
+    word.downcase.scan(Regexp.new(hands[:left])).empty?
   end
 
   #Check to see if left hand can type one word
   def type_with_left_hand?(word)
-    word.downcase.scan(Regexp.new(@hands[:right])).empty?
+    word.downcase.scan(Regexp.new(hands[:right])).empty?
   end
 
   #loop through the dictionary, and return the largest word
   def largest_word_in_dict(dictionary, which_hand)
     largest_one_hand_word = ""
-
-    if which_hand == 'left'
-      dictionary.each do |dict_word|
-        if type_with_left_hand?(dict_word)
-          if dict_word.length > largest_one_hand_word.length
-            largest_one_hand_word = dict_word
-          end
-        end
-      end
-    else
-      dictionary.each do |dict_word|
-        if type_with_right_hand?(dict_word)
-          if dict_word.length > largest_one_hand_word.length
-            largest_one_hand_word = dict_word
-          end
+    dictionary.each do |dict_word|
+      if type_with_one_hand?(which_hand, dict_word)
+        if dict_word.length > largest_one_hand_word.length
+          largest_one_hand_word = dict_word
         end
       end
     end
     largest_one_hand_word
   end
 
+  private
+
+    def type_with_one_hand?(hand,word)
+      send("type_with_#{hand}_hand?".to_sym, word)
+    end
+
 end
+
+
 
 
 
