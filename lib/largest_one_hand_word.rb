ff3182772c4f55c2f0d7f77c03db2cda2fab423e
Nathan Hall <nathan.mh@gmail.com>
2015-01-13 18:01:22 -0600

    Without Solution

--- lib/largest_one_hand_word.rb
+++ lib/largest_one_hand_word.rb
@@ -2,47 +2,7 @@
 require_relative '../tale_of_two_cities.rb'
 
 class LargestWord
-  attr_reader :hands
-  attr_accessor :dictionary
-
   def initialize(dict)
-    @dictionary = dict.split(" ")
-    @hands = {left: "[qwertyasdfgzxcv]", right: '[uiophjklbnm]'}
   end
-
-  #Check to see if right hand can type one word
-  def type_with_right_hand?(word)
-    word.downcase.scan(Regexp.new(@hands[:left])).empty?
-  end
-
-  #Check to see if left hand can type one word
-  def type_with_left_hand?(word)
-    word.downcase.scan(Regexp.new(@hands[:right])).empty?
-  end
-
-  #loop through the dictionary, and return the largest word
-  def largest_word_in_dict(dictionary, which_hand)
-    largest_one_hand_word = ""
-
-    if which_hand == 'left'
-      dictionary.each do |dict_word|
-        if type_with_left_hand?(dict_word)
-          if dict_word.length > largest_one_hand_word.length
-            largest_one_hand_word = dict_word
-          end
-        end
-      end
-    else
-      dictionary.each do |dict_word|
-        if type_with_right_hand?(dict_word)
-          if dict_word.length > largest_one_hand_word.length
-            largest_one_hand_word = dict_word
-          end
-        end
-      end
-    end
-    largest_one_hand_word
-  end
-
 end
 
